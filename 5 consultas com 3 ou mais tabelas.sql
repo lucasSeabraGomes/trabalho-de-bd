0f87110abd5dce8b8a6b6d882ea8b7e27be7f64f
lucasSeabraGomes <39420989+lucasSeabraGomes@users.noreply.github.com>
2019-07-01 19:43:28 -0300

    Add files via upload

--- 5 consultas com 3 ou mais tabelas.sql
+++ 5 consultas com 3 ou mais tabelas.sql
@@ -1,4 +1,10 @@
 --  consulta os nomes dos tipos de equipamento presentes em uma atividade
--- π atividade.idatividade,tipoEq.nome (σ atividade_has_equipamento.idequipamento= equipamento.idequipamento ((equipamento*tipoEq)X(atividade_has_equipamento*atividade))
+-- na algebra relacional temos a busca: π atividade.idatividade,tipoEq.nome (σ atividade_has_equipamento.idequipamento= equipamento.idequipamento ((equipamento*tipoEq)X(atividade_has_equipamento*atividade))
 select atividade.idatividade,tipoEq.nome from (equipamento INNER JOIN tipoEq on equipamento.tipoEq_idtipoEq=tipoEq.idtipoEq) ,(atividade_has_equipamento INNER JOIN atividade on atividade_has_equipamento.atividade_idatividade=atividade.idatividade ) where atividade_has_equipamento.equipamento_idequipamento= equipamento.idequipamento;
 
+--  consulta os nomes dos alunos que participaram de atividades de cada instrutor
+-- na algebra relacional temos a busca: π pessoa.nome (σ escoteiro_participa_atividade.atividade_idatividade=instrutor_administra_atividade.atividade_idatividade (((pessoa*escoteiro)*escoteiro_participa_atividade)X(instrutor*instrutor_administra_atividade))
+select instrutor.pessoa_cpf,pessoa.nome from pessoa,escoteiro,instrutor,instrutor_administra_atividade,escoteiro_participa_atividade where escoteiro.pessoa_cpf =escoteiro_participa_atividade.escoteiro_pessoa_cpf && escoteiro_participa_atividade.atividade_idatividade=instrutor_administra_atividade.atividade_idatividade && escoteiro.pessoa_cpf=pessoa.cpf && instrutor.pessoa_cpf=instrutor_administra_atividade.instrutor_pessoa_cpf;
+-- consulta a descrição das doenças cronicas de determinado escoteiro x
+-- na algebra relacional:π doencaCronica.descricao (σ pessoa.nome="x"&& pessoa.cpf=escoteiro_has_donecaCronica.escoteiro_pessoa_cpf((doencaCronica*escoteiro_has_doecaCronica)X pessoa)
+select doencacronica.descricao from doencacronica,escoteiro_has_doencacronica,pessoa where pessoa.nome='aa' && pessoa.cpf=escoteiro_has_doencacronica.escoteiro_pessoa_cpf && doencaCronica.id=escoteiro_has_doencacronica.doencaCronica_id;
