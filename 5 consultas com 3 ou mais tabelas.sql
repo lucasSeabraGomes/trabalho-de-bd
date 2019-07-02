ebba255ae6af94cdd9d38ef76a1353756a3b5e4d
lucasSeabraGomes <39420989+lucasSeabraGomes@users.noreply.github.com>
2019-07-02 00:18:07 -0300

    Add files via upload

--- 5 consultas com 3 ou mais tabelas.sql
+++ 5 consultas com 3 ou mais tabelas.sql
@@ -7,4 +7,7 @@ select atividade.idatividade,tipoEq.nome from (equipamento INNER JOIN tipoEq on 
 select instrutor.pessoa_cpf,pessoa.nome from pessoa,escoteiro,instrutor,instrutor_administra_atividade,escoteiro_participa_atividade where escoteiro.pessoa_cpf =escoteiro_participa_atividade.escoteiro_pessoa_cpf && escoteiro_participa_atividade.atividade_idatividade=instrutor_administra_atividade.atividade_idatividade && escoteiro.pessoa_cpf=pessoa.cpf && instrutor.pessoa_cpf=instrutor_administra_atividade.instrutor_pessoa_cpf;
 -- consulta a descrição das doenças cronicas de determinado escoteiro x
 -- na algebra relacional:π doencaCronica.descricao (σ pessoa.nome="x"&& pessoa.cpf=escoteiro_has_donecaCronica.escoteiro_pessoa_cpf((doencaCronica*escoteiro_has_doecaCronica)X pessoa)
-select doencacronica.descricao from doencacronica,escoteiro_has_doencacronica,pessoa where pessoa.nome='aa' && pessoa.cpf=escoteiro_has_doencacronica.escoteiro_pessoa_cpf && doencaCronica.id=escoteiro_has_doencacronica.doencaCronica_id;
+select doencacronica.descricao from doencacronica,escoteiro_has_doencacronica,pessoa where pessoa.nome='x' && pessoa.cpf=escoteiro_has_doencacronica.escoteiro_pessoa_cpf && doencaCronica.id=escoteiro_has_doencacronica.doencaCronica_id;
+-- cunsulta de todos os equipamentos comprados por determinado administrador
+-- π equipamento.id(σ compra.id=equipamento.compra_id && pessoa.cpf=compra.administrador_pessoa_cpf && pessoa.nome='x'(compra X pessoa X equipamento))
+select equipamento.idequipamento from compra,equipamento,pessoa where pessoa.cpf=compra.administrador_pessoa_cpf && compra.idcompra=equipamento.compra_idcompra && pessoa.nome='x';
